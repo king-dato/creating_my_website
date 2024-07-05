5632d75ca4654b5fc57dc78e662cc9650f3763d6
king-dato <king_dato@yahoo.com>
2024-07-04 23:20:35 -0400

    update

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// function sayHello() {
+//   console.log("Hello From Function");
+// }
+
+// sayHello();
+
+// console.log("hello World");
+
+let color = "green";
+
+if (color === "green") {
+  console.log("you may go");
+} else {
+  console.log("you should stop");
+}
