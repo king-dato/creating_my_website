dd36c6270f353aa425fe76fc54d6c097b2fa792a
king-dato <king_dato@yahoo.com>
2024-07-09 22:51:46 -0400

    Update 2

--- js/script.js
+++ js/script.js
@@ -6,10 +6,19 @@
 
 // console.log("hello World");
 
-let color = "green";
+// let color = "red";
 
-if (color === "green") {
-  console.log("you may go");
-} else {
-  console.log("you should stop");
+// if (color === "green") {
+// console.log("you may go");
+// } else {
+// console.log("you should stop");
+// }
+
+// global scope
+let x = 5;
+
+if (true) {
+  console.log("This is the block scope", x);
 }
+
+console.log("This is the global scope", x);
