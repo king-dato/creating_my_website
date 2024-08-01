2bb483ffdca55e63b7eea7fcf7282eab9057230e
king-dato <king_dato@yahoo.com>
2024-08-01 15:06:52 -0400

    commit with the modal

--- js/script.js
+++ js/script.js
@@ -44,16 +44,41 @@ let x = 5;
 //   alert("Hello From External Js");
 // }
 
-const clients = [
-  "Mango",
-  "Poly",
-  "Ajax",
-  "samsung",
-  "jave",
-  "america",
-  "goliath",
-];
+// const clients = [
+//   "Mango",
+//   "Poly",
+//   "Ajax",
+//   "samsung",
+//   "jave",
+//   "america",
+//   "goliath",
+// ];
 
-for (let i = 1; i < clients.length; i + 3) {
-  console.log(clients[i]);
-}
+// for (let i = 1; i < clients.length; i ++) {
+//   console.log(clients[i]);
+// }
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
