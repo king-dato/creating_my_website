9ceef8dddde2983d417206749bcb985d98fcd76e
king-dato <king_dato@yahoo.com>
2024-10-08 19:31:40 +0800

    Update

---
+++ app/src/Header.jsx
@@ -0,0 +1,25 @@
+function Header() {
+  return (
+    <header>
+      My website im going to test
+      <nav>
+        <ul>
+          <li>
+            <a href="#"></a> Home
+          </li>
+          <li>
+            <a href="#"> About</a>
+          </li>
+          <li>
+            <a href="#">Services </a>{" "}
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
